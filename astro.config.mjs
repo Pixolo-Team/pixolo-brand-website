bfd052d6f9863f11dc9174443afeda28e92f5213
gerejakajal <gerejakajal207@gmail.com>
2025-11-17 20:52:55 +0530

    Fixed Toolbar positioning on xl and 2xl and responsiveness

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
