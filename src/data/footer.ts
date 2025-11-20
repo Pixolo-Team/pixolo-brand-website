300b94bb095708ff61ab55e0add3e9d205a7b024
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2025-11-20 17:06:57 +0530

    fixes and optimised the footer section

---
+++ src/data/footer.ts
@@ -0,0 +1,10 @@
+// OTHERS //
+import { siteInfo } from "./siteInfo";
+
+export const footerData = {
+  headerText:
+    "Every line of code we write carries our story of curiosity, courage, and a shared laugh between deadlines.",
+  toolBadgeText: "Make the Logo Bigger",
+  logoMessage: "We code better than we socialize. It’s your move.",
+  socialLinks: siteInfo.socialLinks,
+};
---
+++ src/data/siteInfo.ts
@@ -0,0 +1,12 @@
+export const siteInfo = {
+  socialLinks: [
+    {
+      iconName: "instagram",
+      link: "https://www.instagram.com/pixolotechnologies/",
+    },
+    {
+      iconName: "linkedin",
+      link: "https://in.linkedin.com/company/pixolo-technologies",
+    },
+  ],
+};
