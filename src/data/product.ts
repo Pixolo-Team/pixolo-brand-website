9fd2f39d3a151aafcab51dfede3cd44973f72b5c
sneha-g0512 <galanisneha0512@gmail.com>
2025-11-21 17:09:12 +0530

    Added .ts file as product.ts and fixed most of the comments for product section

---
+++ src/data/product.ts
@@ -0,0 +1,51 @@
+export const productData = {
+  productIcon: "product-icon",
+  toolBadgeText: "Our in-house creations.",
+  textWrapperBuiltWith: "Built with",
+  titleWrapperImage: "title-wrapper-image.png",
+  titleWrapperAlt: "Title Wrapper Image",
+  textWrapperByPixolo: "by Pixolo",
+  titleWrapperMessage: "Where our creativity meets real-world impact.",
+  products: [
+    {
+      companyLogo: "zizo.png",
+      productImage: "product-image.png",
+      productName: "Zizo",
+      productdescriptionStart: "A smart football team management app that",
+      productdescriptionBold: "helps coaches, players, and clubs",
+      productdescriptionEnd:
+        "organize matches, track stats, and manage their teams seamlessly.",
+      href: "/",
+    },
+    {
+      companyLogo: "configo.png",
+      productImage: "product-image.png",
+      productName: "Configo",
+      productdescriptionStart: "A smart football team management app that",
+      productdescriptionBold: "helps coaches, players, and clubs",
+      productdescriptionEnd:
+        "organize matches, track stats, and manage their teams seamlessly.",
+      href: "/",
+    },
+    {
+      companyLogo: "weddin-go.png",
+      productImage: "product-image.png",
+      productName: "WeddinGo",
+      productdescriptionStart: "A smart football team management app that",
+      productdescriptionBold: "helps coaches, players, and clubs",
+      productdescriptionEnd:
+        "organize matches, track stats, and manage their teams seamlessly.",
+      href: "/",
+    },
+    {
+      companyLogo: "neevo-design-system.png",
+      productImage: "product-image.png",
+      productName: "Neevo Design System",
+      productdescriptionStart: "A smart football team management app that",
+      productdescriptionBold: "helps coaches, players, and clubs",
+      productdescriptionEnd:
+        "organize matches, track stats, and manage their teams seamlessly.",
+      href: "/",
+    },
+  ],
+};
