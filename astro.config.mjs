82229d3f03b28da0d25c221b70a09799996a1212
Mohd hussain <90891392+MohdhussainSunesara@users.noreply.github.com>
2025-11-24 16:56:29 +0530

    Contact Section - UI Development (#20)
    * started with Contact section
    
    * mobile responsive made for contac section
    
    * tablet responsive
    
    * fixes
    
    * responsive fixes
    
    * hover state and tooltip added
    
    * hover added on Map view
    
    * tooltip fix
    
    * ui testing comments fixes
    
    * ui testing comment fixes
    
    * location map and qc point fixed
    
    * copy to clipboard functionality added
    
    * ui testing comment fixes
    
    * qc fixes
    
    * pr fixes
    
    * package lock added to gitignore
    
    * .gitignore is now working
    
    * map image removed
    
    * pr fixes
    
    * pr fixes
    
    * gradient variable added
    
    * pr fixes
    
    * conflicts resolved
    
    * Fixes in code
    
    * Issues Fixed
    
    ---------
    
    Co-authored-by: Abhay Amin <contactabhay2@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 // OTHERS //
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [icon()],
 });
