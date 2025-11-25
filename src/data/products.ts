bf945d838848805baf88b2431d9703cc71c11b52
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2025-11-25 20:08:49 +0530

    pr fixes

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,41 +1,41 @@
 export const homeProductsDetails = {
   productIcon: "product-icon",
   toolBadgeText: "Our in-house creations.",
-  textWrapperBuiltWith: "Built with",
-  titleWrapperImage: "heart-outline-icon.png",
-  titleWrapperAlt: "Title Wrapper Image",
-  textWrapperByPixolo: "by Pixolo",
-  titleWrapperMessage: "Where our creativity meets real-world impact.",
+  titlePrefix: "Built with",
+  titleIcon: "heart-outline-icon.png",
+  titleIconAlt: "Title Wrapper Image",
+  titleSuffix: "by Pixolo",
+  subTitle: "Where our creativity meets real-world impact.",
 };
 
 export const productItems = [
   {
-    logo: "zizo.png",
-    image: "thumbnail.jpg",
+    logo: "zizo-logo.png",
+    thumbnail: "zizo-thumbnail.jpg",
     name: "Zizo",
     description:
       "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
     href: "/",
   },
   {
-    logo: "configo.png",
-    image: "thumbnail.jpg",
+    logo: "configo-logo.png",
+    thumbnail: "configo-thumbnail.jpg",
     name: "Configo",
     description:
       "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
     href: "/",
   },
   {
-    logo: "weddin-go.png",
-    image: "thumbnail.jpg",
+    logo: "weddingo-logo.png",
+    thumbnail: "weddingo-thumbnail.jpg",
     name: "WeddinGo",
     description:
       "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
     href: "/",
   },
   {
-    logo: "neevo-design-system.png",
-    image: "thumbnail.jpg",
+    logo: "neevo-logo.png",
+    thumbnail: "neevo-thumbnail.jpg",
     name: "Neevo Design System",
     description:
       "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
