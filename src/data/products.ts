edebea4a624463a1cd993c8a40318615610f1c0f
galanisneha0512 <galanisneha0512@gmail.com>
2025-11-25 20:13:30 +0530

    [WIP] Product Item - UI Development (#31)
    * Created two new components for product item and rendered temporarily through index.astro
    
    * Added Mobile CSS to product item
    
    * Added responsiveness to product item and resolved most of the comments
    
    * Altered css based on the comments and resolved them
    
    * Created Product Section And Added Mobile CSS
    
    * Made the product section responsive
    
    * Fixed Hover animations for product item
    
    * Added .ts file as product.ts and fixed most of the comments for product section
    
    * Fixed width issue within Product Item
    
    * Resolved comments based on UI testing
    
    * Made changes in product item and product section based on comments
    
    * fixes
    
    * fixes
    
    * pr fixes
    
    * pr fixes
    
    ---------
    
    Co-authored-by: Mohdhussainsunesara <m.sunesara@somaiya.edu>

---
+++ src/data/products.ts
@@ -0,0 +1,44 @@
+export const homeProductsDetails = {
+  toolBadgeIcon: "product-icon",
+  toolBadgeText: "Our in-house creations.",
+  titlePrefix: "Built with",
+  titleIcon: "heart-outline-icon.png",
+  titleIconAlt: "Pixolo Products Heart",
+  titleSuffix: "by Pixolo",
+  subTitle: "Where our creativity meets real-world impact.",
+};
+
+export const productItems = [
+  {
+    logo: "zizo-logo.png",
+    thumbnail: "zizo-thumbnail.jpg",
+    name: "Zizo",
+    description:
+      "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
+    href: "/",
+  },
+  {
+    logo: "configo-logo.png",
+    thumbnail: "configo-thumbnail.jpg",
+    name: "Configo",
+    description:
+      "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
+    href: "/",
+  },
+  {
+    logo: "weddingo-logo.png",
+    thumbnail: "weddingo-thumbnail.jpg",
+    name: "WeddinGo",
+    description:
+      "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
+    href: "/",
+  },
+  {
+    logo: "neevo-logo.png",
+    thumbnail: "neevo-thumbnail.jpg",
+    name: "Neevo Design System",
+    description:
+      "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
+    href: "/",
+  },
+];
