52a3a0b46d8921c56e7a0d21dde15a8fd793d2ef
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2025-11-25 18:20:32 +0530

    fixes

--- src/data/products.ts
+++ src/data/products.ts
@@ -5,40 +5,40 @@ export const homeProductsDetails = {
   titleWrapperImage: "heart-outline-icon.png",
   titleWrapperAlt: "Title Wrapper Image",
   textWrapperByPixolo: "by Pixolo",
-  titleWrapperMessage: "Where our creativity meets real-world impact."
+  titleWrapperMessage: "Where our creativity meets real-world impact.",
 };
 
-export const productItems= [
-    {
-      companyLogo: "zizo.png",
-      companyName:"This is the logo for Zizo Company",
-      imageName: "thumbnail.jpg",
-      name: "Zizo",
-      description : "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
-      href: "/",
-    },
-    {
-      companyLogo: "configo.png",
-      companyName:"This is the logo for Configo Company",
-      imageName: "thumbnail.jpg",
-      name: "Configo",
-      description : "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
-      href: "/",
-    },
-    {
-      companyLogo: "weddin-go.png",
-      companyName:"This is the logo for WeddinGo Company",
-      imageName: "thumbnail.jpg",
-      name: "WeddinGo",
-      description : "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
-      href: "/",
-    },
-    {
-      companyLogo: "neevo-design-system.png",
-      companyName:"This is the logo for Neevo Design System",
-      imageName: "thumbnail.jpg",
-      name: "Neevo Design System",
-      description : "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
-      href: "/"
-    }
-  ]
+export const productItems = [
+  {
+    logo: "zizo.png",
+    image: "thumbnail.jpg",
+    name: "Zizo",
+    description:
+      "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
+    href: "/",
+  },
+  {
+    logo: "configo.png",
+    image: "thumbnail.jpg",
+    name: "Configo",
+    description:
+      "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
+    href: "/",
+  },
+  {
+    logo: "weddin-go.png",
+    image: "thumbnail.jpg",
+    name: "WeddinGo",
+    description:
+      "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
+    href: "/",
+  },
+  {
+    logo: "neevo-design-system.png",
+    image: "thumbnail.jpg",
+    name: "Neevo Design System",
+    description:
+      "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
+    href: "/",
+  },
+];
