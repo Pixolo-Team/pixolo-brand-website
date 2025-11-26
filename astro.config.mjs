8ec1e310ee64aadc22d51fd2ba3d7d8783ab18e1
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2025-11-26 11:17:17 +0530

    contact form fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
