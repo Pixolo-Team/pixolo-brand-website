b9851da0c1341709a9999b1693a8b36ab16f00d8
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2025-11-26 19:45:02 +0530

    pr fixes

---
+++ src/data/clients.ts
@@ -0,0 +1,22 @@
+// Clients data that can be reused across the application
+export const clientSectionDetails = {
+  badgeTitle: "Powering Ideas into Impact",
+  title: "Brands that believe in our code",
+};
+
+export const clientsItems = [
+  { name: "Reliance Digital", darkImage: "reliance.png", lightImage: "reliance.png" },
+  { name: "Times Of India", darkImage: "times-of-india.png", lightImage: "times-of-india.png" },
+  { name: "Ceat", darkImage: "ceat.png", lightImage: "ceat.png" },
+  { name: "Pango", darkImage: "pango.png", lightImage: "pango.png" },
+  { name: "Eighty Days", darkImage: "eighty-days.png", lightImage: "eighty-days.png" },
+  { name: "Too Yumm", darkImage: "too-yumm.png", lightImage: "too-yumm.png" },
+  {
+    name: "SAI International School",
+    darkImage: "sai-international.png",
+    lightImage: "sai-international.png",
+  },
+  { name: "Zizo", darkImage: "zizo.png", lightImage: "zizo.png" },
+  { name: "Skorost United", darkImage: "skorost.png", lightImage: "skorost.png" },
+  { name: "Fcb", darkImage: "fcb.png", lightImage: "fcb.png" },
+];
