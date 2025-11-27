9ab7ecb539510698b38db4bae5d2243021e8bcde
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2025-11-27 15:49:07 +0530

    check icon on copy click added

--- src/scripts/contact-copy.ts
+++ src/scripts/contact-copy.ts
@@ -1,14 +1,36 @@
-/**
- * Initialize copy buttons
- */
-export function initCopyButtons() {
-  const buttons = document.querySelectorAll(".copy-btn");
+/** Initialize copy buttons */
+export const initCopyButtons = () => {
+  const buttons = document.querySelectorAll<HTMLElement>(".copy-btn");
 
   buttons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
+    const text = btn.dataset.copy;
+    if (!text) return;
+
+    // Get both the Icons
+    const copyIcon = btn.querySelector<HTMLElement>(".icon-copy");
+    const checkIcon = btn.querySelector<HTMLElement>(".icon-check");
+
+    if (!copyIcon || !checkIcon) return;
+
+    btn.addEventListener("click", async (e) => {
       e.preventDefault();
-      const value = btn.getAttribute("data-copy");
-      if (value) navigator.clipboard.writeText(value);
+      e.stopPropagation();
+
+      // Copy Text
+      await navigator.clipboard.writeText(text);
+
+      // Hide copy icon
+      copyIcon.classList.add("hidden");
+      // Show check icon
+      checkIcon.classList.remove("hidden");
+
+      // Auto-reset on setTimeout
+      setTimeout(() => {
+        // Hide copy icon
+        checkIcon.classList.add("hidden");
+        // Show copy icon
+        copyIcon.classList.remove("hidden");
+      }, 3000);
     });
   });
-}
+};
