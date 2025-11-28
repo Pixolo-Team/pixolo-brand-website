1a2d92f55e5210e93dd1b167beac21dbd0a0fc04
gerejakajal <gerejakajal207@gmail.com>
2025-11-28 21:32:40 +0530

    Added retainership section and made it responsive

---
+++ src/data/retainership.ts
@@ -0,0 +1,6 @@
+export const retainershipDetails = {
+  headerText: "We lend our best minds to strengthen yours.",
+  stepsTitle: "How it works",
+  contentHeading: "Our retainership ensures ongoing collaboration, support, and lasting value.",
+  employeeCardTitle: "Some of the skilled professionals we provide include:",
+};
