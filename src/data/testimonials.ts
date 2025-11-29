5e50b3c2b5e9dd3616f419c7d0364dd7b60159c2
omisan01 <omkar12mitake@gmail.com>
2025-11-29 16:51:51 +0530

    testimonials

---
+++ src/data/testimonials.ts
@@ -0,0 +1,22 @@
+// Client Details
+export const clientDetails = {
+  profilePicture: "client-one.jpg",
+  name: "Sarah Luprich",
+  description: "EyeCatcher",
+};
+
+// Clients that trust in us
+export const clients = [
+  {
+    profilePicture: "client-one.jpg",
+    description: "This is profile picture of our client",
+  },
+  {
+    profilePicture: "client-two.jpg",
+    description: "This is profile picture of our client",
+  },
+  {
+    profilePicture: "client-one.jpg",
+    description: "This is profile picture of our client",
+  },
+];
