3e6fc4acd7e4376726565b667288c627bc52009e
nagdevtaniya <nagdevtaniya@gmail.com>
2025-12-05 00:18:16 +0530

    Fixed pr comments

---
+++ src/data/blogs.ts
@@ -0,0 +1,40 @@
+export const blogSectionDetails = {
+  label: "Code & Culture",
+  heading: "Stories and trends shaping how technology connects us.",
+  loadMoreButton: "Load More Blogs",
+};
+
+export const blogItems = [
+  {
+    imageSrc: "/images/blogs/blog-card.jpg",
+    category: "Design",
+    date: "6 Nov 2025",
+    title: "Get expert design advice to create experiences.",
+    author: "Omkar Mitake",
+    href: "#",
+  },
+  {
+    imageSrc: "/images/blogs/blog-card.jpg",
+    category: "Design",
+    date: "6 Nov 2025",
+    title: "Get expert design advice to create experiences.",
+    author: "Omkar Mitake",
+    href: "#",
+  },
+  {
+    imageSrc: "/images/blogs/blog-card.jpg",
+    category: "Design",
+    date: "6 Nov 2025",
+    title: "Get expert design advice to create experiences.",
+    author: "Omkar Mitake",
+    href: "#",
+  },
+  {
+    imageSrc: "/images/blogs/blog-card.jpg",
+    category: "Design",
+    date: "6 Nov 2025",
+    title: "Get expert design advice to create experiences.",
+    author: "Omkar Mitake",
+    href: "#",
+  },
+];
