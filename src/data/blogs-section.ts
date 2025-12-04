18f0d30d924de8be7bafcbd57ac1314a62c7e7a5
nagdevtaniya <nagdevtaniya@gmail.com>
2025-12-04 22:39:55 +0530

    Added Cards and made it responsive

--- src/data/blogs-section.ts
+++ src/data/blogs-section.ts
@@ -2,4 +2,38 @@ export const blogSectionText = {
   label: "Code & Culture",
   heading: "Stories and trends shaping how technology connects us.",
   loadMoreButton: "Load More Blogs",
+  cards: [
+    {
+      imageSrc: "/images/blogs/blog-card.jpg",
+      category: "Design",
+      date: "6 Nov 2025",
+      title: "Get expert design advice to create experiences.",
+      author: "Omkar Mitake",
+      href: "#",
+    },
+    {
+      imageSrc: "/images/blogs/blog-card.jpg",
+      category: "Design",
+      date: "6 Nov 2025",
+      title: "Get expert design advice to create experiences.",
+      author: "Omkar Mitake",
+      href: "#",
+    },
+    {
+      imageSrc: "/images/blogs/blog-card.jpg",
+      category: "Design",
+      date: "6 Nov 2025",
+      title: "Get expert design advice to create experiences.",
+      author: "Omkar Mitake",
+      href: "#",
+    },
+    {
+      imageSrc: "/images/blogs/blog-card.jpg",
+      category: "Design",
+      date: "6 Nov 2025",
+      title: "Get expert design advice to create experiences.",
+      author: "Omkar Mitake",
+      href: "#",
+    },
+  ],
 };
