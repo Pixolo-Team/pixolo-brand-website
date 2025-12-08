47e639c26e4695a514df24c22908ffcb8b436576
Jayant Singh <jayantsinghhh89@gmail.com>
2025-12-08 18:17:54 +0530

    Client Issue Component Added

--- src/data/case-studies/1.ts
+++ src/data/case-studies/1.ts
@@ -4,3 +4,24 @@ export const clientNumbers = [
   { title: "Customer Support", value: "24/7" },
   { title: "Higher Conversion Rates", value: "15%" },
 ];
+
+export const clientIssues = [
+  {
+    title: "Slow Loading",
+    description: "There were lots of slow issues on loading",
+    issue: "Loading speed",
+    stat: "60s",
+  },
+  {
+    title: "High Latency",
+    description: "Network latency caused significant delays",
+    issue: "Reponse time",
+    stat: "120ms",
+  },
+  {
+    title: "Data Overload",
+    description: "Excessive data led to performance issues",
+    issue: "Processing time",
+    stat: "90s",
+  },
+];
