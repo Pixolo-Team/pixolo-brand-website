a6e17baf556f19ec8d5c8e182c5f87fb6317ebf7
Jayant Singh <jayantsinghhh89@gmail.com>
2025-12-09 17:20:28 +0530

    Case Studies - Solutions Section - Component Added

--- src/data/case-studies/1.ts
+++ src/data/case-studies/1.ts
@@ -4,3 +4,46 @@ export const clientNumbers = [
   { title: "Customer Support", value: "24/7" },
   { title: "Higher Conversion Rates", value: "15%" },
 ];
+
+export const clientSolutions = [
+  {
+    title: "A Clear, Predictable Navigation System",
+    description:
+      "To reduce user confusion and shorten task flows, we redesigned the information architecture from the ground up.",
+  },
+  {
+    title: "A Clear, Predictable Navigation System",
+    description:
+      "To reduce user confusion and shorten task flows, we redesigned the information architecture from the ground up.",
+  },
+  {
+    title: "A Clear, Predictable Navigation System",
+    description:
+      "To reduce user confusion and shorten task flows, we redesigned the information architecture from the ground up.",
+  },
+  {
+    title: "A Clear, Predictable Navigation System",
+    description:
+      "To reduce user confusion and shorten task flows, we redesigned the information architecture from the ground up.",
+  },
+  {
+    title: "A Clear, Predictable Navigation System",
+    description:
+      "To reduce user confusion and shorten task flows, we redesigned the information architecture from the ground up.",
+  },
+  {
+    title: "Responsive Design for All Devices",
+    description:
+      "Ensuring a seamless experience across devices, we implemented a fully responsive layout that adapts to various screen sizes",
+  },
+  {
+    title: "Enhanced Accessibility Features",
+    description:
+      "To cater to all users, we integrated new accessibility options, making our platform usable for individuals with diverse needs.",
+  },
+  {
+    title: "Streamlined User Onboarding Process",
+    description:
+      "We simplified the onboarding experience to help new users get started quickly and confidently with intuitive tutorials.",
+  },
+];
