800ebd98fdeb58e201437ba78f8d1d601333a0fa
jayants1903 <87685806+jayants1903@users.noreply.github.com>
2025-12-09 19:17:13 +0530

    Case Studies - Solutions Section (#80)
    * Case Studies - Solutions section created
    
    * Case Studies - Solutions Component (#81)
    
    * Case Studies - Solutions Section - Component Added
    
    * ClientSolution Component Fixes
    
    * Solution Component Fixes
    
    * Solutions Component
    
    * Update Solutions.astro
    
    ---------
    
    Co-authored-by: Abhay Amin <pixoloproductions@gmail.com>

--- src/data/case-studies/1.ts
+++ src/data/case-studies/1.ts
@@ -5,6 +5,49 @@ export const clientNumbers = [
   { title: "Higher Conversion Rates", value: "15%" },
 ];
 
+export const clientSolutions = [
+  {
+    title: "A Clear, Predictable Navigation System",
+    description:
+      "To reduce user confusion and shorten task flows, we redesigned the information architecture from the ground up.",
+  },
+  {
+    title: "A Clear, Predictable Navigation System",
+    description:
+      "To reduce user confusion and shorten task flows, we redesigned the information architecture from the ground up.",
+  },
+  {
+    title: "A Clear, Predictable Navigation System",
+    description:
+      "To reduce user confusion and shorten task flows, we redesigned the information architecture from the ground up.",
+  },
+  {
+    title: "A Clear, Predictable Navigation System",
+    description:
+      "To reduce user confusion and shorten task flows, we redesigned the information architecture from the ground up.",
+  },
+  {
+    title: "A Clear, Predictable Navigation System",
+    description:
+      "To reduce user confusion and shorten task flows, we redesigned the information architecture from the ground up.",
+  },
+  {
+    title: "Responsive Design for All Devices",
+    description:
+      "Ensuring a seamless experience across devices, we implemented a fully responsive layout that adapts to various screen sizes",
+  },
+  {
+    title: "Enhanced Accessibility Features",
+    description:
+      "To cater to all users, we integrated new accessibility options, making our platform usable for individuals with diverse needs.",
+  },
+  {
+    title: "Streamlined User Onboarding Process",
+    description:
+      "We simplified the onboarding experience to help new users get started quickly and confidently with intuitive tutorials.",
+  }
+];
+
 export const clientIssues = [
   {
     title: "Slow Loading",
@@ -25,6 +68,7 @@ export const clientIssues = [
     stat: "90s",
   },
 ];
+
 export const aboutClient = {
   aboutPointers: [
     {
