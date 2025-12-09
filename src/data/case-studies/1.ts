f828f4c55e3d010c9d06f519ebe8c8c331124ff3
jayants1903 <87685806+jayants1903@users.noreply.github.com>
2025-12-09 18:14:32 +0530

    Case Studies: About the Client Section (#76)
    * created the files for About the client Section
    
    * About The Client Section Added
    
    * About the Client section added
    
    * About the client Data & Image Path Fixes
    
    * Section Header Component created & added in About The Client Section
    
    * About the client Mapping Fixes & Section Header Fixes
    
    * About the client fixes
    
    * About the client Fixes

--- src/data/case-studies/1.ts
+++ src/data/case-studies/1.ts
@@ -4,3 +4,25 @@ export const clientNumbers = [
   { title: "Customer Support", value: "24/7" },
   { title: "Higher Conversion Rates", value: "15%" },
 ];
+
+export const aboutClient = {
+  aboutPointers: [
+    {
+      title: "Football Club Barcelona",
+      description:
+        "An app centred around providing users with a complete experience of Conscient Football tournaments which includes the fixtures, results, groups and an activity room. Users can post photos, upload status and use for downloading images and videos of various matches.",
+    },
+    {
+      title: "Brand Background",
+      description:
+        "The Barcelona Football Academy delivers elite football training inspired by top European methodologies. Designed for young players worldwide, the academy focuses on skill development, discipline, and long-term growth across all levels.",
+    },
+  ],
+  aboutImages: {
+    mainImage: "/images/case-studies/1/client-barcelona-logo.jpg",
+    otherImages: [
+      "/images/case-studies/1/client-barcelona-app.jpg",
+      "/images/case-studies/1/client-barcelona-player.jpg",
+    ],
+  },
+};
