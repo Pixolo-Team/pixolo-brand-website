c94adf57405d53422ce4f5bc8ef2fca63386e068
jayants1903 <87685806+jayants1903@users.noreply.github.com>
2025-12-09 18:48:20 +0530

    Case Studies - Client Issues Section (#77)
    * Case Studies - Client Issues Section
    
    * Client Issue Component
    
    * Client Issue Component Added
    
    * Client Issue Component Gap Fixes
    
    * Client Issue Component Code Fix
    
    * Client Issue Component Fixes
    
    * fix
    
    * Client Issues Section Added
    
    * Client Issue Component Fixes
    
    ---------
    
    Co-authored-by: omisan01 <omkar12mitake@gmail.com>
    Co-authored-by: Omkar Mitake <80113993+omisan01@users.noreply.github.com>
    Co-authored-by: Abhay Amin <pixoloproductions@gmail.com>

--- src/data/case-studies/1.ts
+++ src/data/case-studies/1.ts
@@ -5,6 +5,26 @@ export const clientNumbers = [
   { title: "Higher Conversion Rates", value: "15%" },
 ];
 
+export const clientIssues = [
+  {
+    title: "Slow Loading",
+    description: "There were lots of slow issues on loading",
+    issue: "Loading speed",
+    stat: "60s",
+  },
+  {
+    title: "High Latency",
+    description: "Network latency caused significant delays",
+    issue: "Reponse time",
+    stat: "120ms",
+  },
+  {
+    title: "Data Overload",
+    description: "Excessive data led to performance issues",
+    issue: "Processing time",
+    stat: "90s",
+  },
+];
 export const aboutClient = {
   aboutPointers: [
     {
