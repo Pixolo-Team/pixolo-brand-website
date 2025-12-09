67944b1c05066062767e62b495da1d0d5776b4ad
Jayant Singh <jayantsinghhh89@gmail.com>
2025-12-09 14:26:24 +0530

    Client Vision Section Image Fixes

--- src/data/case-studies/1.ts
+++ src/data/case-studies/1.ts
@@ -5,21 +5,26 @@ export const clientNumbers = [
   { title: "Higher Conversion Rates", value: "15%" },
 ];
 
-export const clientVisions = [
-  {
-    title: "Fast Loading",
-    description: "Pages laod instantly, keeping visitors engaged instead of frustrated",
-  },
-  {
-    title: "Mobile Optimization",
-    description: "Seamless experience across all devices, ensuring accessibility on the go%",
-  },
-  {
-    title: "User-Friendly Interface",
-    description: "Intuitive design that enhances navigation and reduces bounce rates",
-  },
-  {
-    title: "SEO Friendly",
-    description: "Built with SEO best practices to improve visibility in search engine",
-  },
-];
+export const clientVisionSection = {
+  mainImage: "/images/case-studies/1/client-fcbarcelona-club.jpg",
+  imageAlt: "FC Barcelona Image",
+
+  clientVisions: [
+    {
+      title: "Fast Loading",
+      description: "Pages laod instantly, keeping visitors engaged instead of frustrated",
+    },
+    {
+      title: "Mobile Optimization",
+      description: "Seamless experience across all devices, ensuring accessibility on the go%",
+    },
+    {
+      title: "User-Friendly Interface",
+      description: "Intuitive design that enhances navigation and reduces bounce rates",
+    },
+    {
+      title: "SEO Friendly",
+      description: "Built with SEO best practices to improve visibility in search engine",
+    },
+  ],
+};
