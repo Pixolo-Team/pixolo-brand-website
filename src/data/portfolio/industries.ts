2200e03b7ae58f084d0adb231f3e5ae457ae7c00
nagdevtaniya <nagdevtaniya@gmail.com>
2025-12-12 18:20:24 +0530

    Fully responsive industries section on portfolio page

---
+++ src/data/portfolio/industries.ts
@@ -0,0 +1,28 @@
+export interface IndustryCard {
+  title: string;
+  image: string;
+  tags: string[];
+}
+
+export const industries: IndustryCard[] = [
+  {
+    title: "Sports",
+    image: "/images/IndustriesCard/sports.jpg",
+    tags: ["Zizo", "Sikorst United", "Fc Barcelona"],
+  },
+  {
+    title: "Finance",
+    image: "/images/IndustriesCard/finance.jpg",
+    tags: ["Zizo", "Sikorst United", "App"],
+  },
+  {
+    title: "Digital",
+    image: "/images/IndustriesCard/digital.jpg",
+    tags: ["Zizo", "Sikorst United", "App"],
+  },
+  {
+    title: "Digital",
+    image: "/images/IndustriesCard/digital.jpg",
+    tags: ["Zizo", "Sikorst United", "App"],
+  },
+];
