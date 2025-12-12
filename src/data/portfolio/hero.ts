ca5495d7065c4002e5195dbdac3d16e605ca23cd
nagdevtaniya <nagdevtaniya@gmail.com>
2025-12-12 19:25:59 +0530

    Fixed pr comments

---
+++ src/data/portfolio/hero.ts
@@ -0,0 +1,5 @@
+export const portfolioHero = {
+  tagTitle: "Crafted Highlights",
+  mainHeading: "A showcase of the work that defines our creativity and craft",
+  subHeading: "A dynamic showcase where every build sparks innovation",
+};
