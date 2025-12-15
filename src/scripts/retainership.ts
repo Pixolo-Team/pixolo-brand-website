2a91ecf765f562bce7550a7d5218419d184c34ab
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2025-12-15 17:19:27 +0530

    animations added to retainership section

--- src/scripts/retainership.ts
+++ src/scripts/retainership.ts
@@ -1,3 +1,55 @@
+import { animate, inView, stagger } from "motion";
+
+/** Animate Retainership Section */
+export const animateRetainershipSection = () => {
+  // Detect when the Section comes into the Screen
+  inView("#retainership-section", (retainershipSection) => {
+    // Animate the Section Header Part
+    animate(
+      retainershipSection?.querySelectorAll(".tool-badge"),
+      { opacity: [0, 1], y: ["50px", 0], scaleX: [0.6, 1], rotateX: [120, 0] },
+      { duration: 0.8, delay: 0 },
+    );
+
+    animate(
+      retainershipSection?.querySelectorAll("#header-text"),
+      { opacity: [0, 1], y: ["50px", 0], scaleY: [0.4, 1] },
+      { duration: 0.8, delay: 0.4 },
+    );
+  });
+
+  inView(".step-item", (step) => {
+    animate(
+      step?.querySelectorAll("p"),
+      {
+        opacity: [0, 1],
+        x: [40, 0],
+      },
+      {
+        duration: 0.8,
+        delay: stagger(0.12),
+        easing: "ease-out",
+      },
+    );
+  });
+
+  inView(".retainership-cards", (retainershipCards) => {
+    animate(
+      retainershipCards?.querySelectorAll(".retainership-card"),
+      { x: ["50px", 0], opacity: [0, 1] },
+      { duration: 0.8, delay: stagger(0.12) },
+    );
+  });
+
+  inView(".employee-cards", (employeeCards) => {
+    animate(
+      employeeCards?.querySelectorAll("#employee-card"),
+      { y: ["50px", 0], opacity: [0, 1] },
+      { duration: 0.8, delay: stagger(0.12) },
+    );
+  });
+};
+
 /** Function to initialize the scroll line animation */
 export const initScrollLine = () => {
   // Get scroll line element
