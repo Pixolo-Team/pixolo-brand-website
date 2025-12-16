5fcafd90ea82f31ccbb3344dc47709296bf9ceb7
nagdevtaniya <nagdevtaniya@gmail.com>
2025-12-16 17:40:38 +0530

    UI development

---
+++ src/data/portfolio/project-consumption.ts
@@ -0,0 +1,17 @@
+export const projectConsumptionHero = {
+  logo: "/images/portfolio/project-consumption/fcb.png", 
+  
+  title: "FC Barcelona",
+  
+  description: "A project crafted with purpose, creativity & seamless execution",
+  
+  visitButton: {
+    label: "Visit Website",
+    href: "https://www.fcbarcelona.com"
+  },
+  
+  showProjectButton: {
+    label: "Show Project",
+    href: "/projects/fc-barcelona"
+  }
+};
