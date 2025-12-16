c086b8ee577a26589b59378700c8b9a32b2b17d3
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2025-12-16 18:00:57 +0530

    pr fixes

--- src/data/case-studies.ts
+++ src/data/case-studies.ts
@@ -1,22 +1,23 @@
+// Case Study Section Details
 export const caseStudySectionDetails = {
   title: "Smart Solutions to Real Challenges.",
-  badgeDetails: "Case Studies",
-};
+  badgeTitle: "Case Studies",
 
-export const caseStudiesItems = [
-  {
-    id: 1,
-    title: "Simplifying Team Management for Coaches and Players",
-    image: "default.jpg",
-  },
-  {
-    id: 2,
-    title: "Bridging Figma to Code Seamlessly in VS Code",
-    image: "default.jpg",
-  },
-  {
-    id: 3,
-    title: "Making Billings Simpler for Startups",
-    image: "default.jpg",
-  },
-];
+  caseStudyItems: [
+    {
+      id: 1,
+      title: "Simplifying Team Management for Coaches and Players",
+      image: "default.jpg",
+    },
+    {
+      id: 2,
+      title: "Bridging Figma to Code Seamlessly in VS Code",
+      image: "default.jpg",
+    },
+    {
+      id: 3,
+      title: "Making Billings Simpler for Startups",
+      image: "default.jpg",
+    },
+  ],
+};
