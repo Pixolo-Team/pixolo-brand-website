124d9c462b5aa6c15cac500e15b55e918ff214ac
Palak Lund <palaklund123@gmail.com>
2025-12-16 11:04:22 +0530

    made the changes according to the comments

--- src/data/case-studies.ts
+++ src/data/case-studies.ts
@@ -1,29 +1,21 @@
-export interface CaseStudyItem {
-  id: number;
-  title: string;
-  image: string;
-}
-
-export interface CaseStudiesData {
-  caseStudiesItems: CaseStudyItem[];
-}
-
-export const caseStudies: CaseStudiesData = {
-  caseStudiesItems: [
-    {
-      id: 1,
-      title: "Simplifying Team Management for Coaches and Players",
-      image: "default.jpg",
-    },
-    {
-      id: 2,
-      title: "Bridging Figma to Code Seamlessly in VS Code",
-      image: "default.jpg",
-    },
-    {
-      id: 3,
-      title: "Making Billings Simpler for Startups",
-      image: "default.jpg",
-    },
-  ],
+export const caseStudySectionDetails = {
+  title: "Smart Solutions to Real Challenges.",
 };
+
+export const caseStudiesItems = [
+  {
+    id: 1,
+    title: "Simplifying Team Management for Coaches and Players",
+    image: "default.jpg",
+  },
+  {
+    id: 2,
+    title: "Bridging Figma to Code Seamlessly in VS Code",
+    image: "default.jpg",
+  },
+  {
+    id: 3,
+    title: "Making Billings Simpler for Startups",
+    image: "default.jpg",
+  },
+];
