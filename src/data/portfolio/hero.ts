169cea95daa93218c82f246eecb7ae99a7f6b71d
nagdevtaniya <nagdevtaniya@gmail.com>
2025-12-16 18:19:30 +0530

    Minor changes

--- src/data/portfolio/hero.ts
+++ src/data/portfolio/hero.ts
@@ -1,5 +1,5 @@
-export const portfolioHero = {
-  tagTitle: "Crafted Highlights",
-  mainHeading: "A showcase of the work that defines our creativity and craft",
+export const portfolioHeroDetails = {
+  badgeTitle: "Crafted Highlights",
+  title: "A showcase of the work that defines our creativity and craft",
   subHeading: "A dynamic showcase where every build sparks innovation",
 };
