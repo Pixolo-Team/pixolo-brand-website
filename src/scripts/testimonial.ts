b68fbc933159995cb13af6e67645814ce870dd42
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2025-12-18 11:26:05 +0530

    final and all animations added

--- src/scripts/testimonial.ts
+++ src/scripts/testimonial.ts
@@ -1,3 +1,5 @@
+import { animate, inView, stagger } from "motion";
+
 /** Function to initialize testimonial */
 export function initTestimonials() {
   const items = document.querySelectorAll<HTMLElement>(".testimonial-item");
@@ -20,6 +22,9 @@ export function initTestimonials() {
   });
 
   function setActive(activeItem: HTMLElement) {
+    // If Already active then do nothing
+    if (activeItem.classList.contains("active")) return;
+
     items.forEach((item) => item.classList.remove("active"));
     activeItem.classList.add("active");
 
@@ -31,3 +36,37 @@ export function initTestimonials() {
     }, 200);
   }
 }
+
+/** Animate the Testimonials Section */
+export const animateTestimonials = () => {
+  // Detect when the Section comes into the Screen
+  inView("#testimonials-section", (testimonialsSection) => {
+    // Animate the Section Header Part
+    animate(
+      testimonialsSection?.querySelectorAll(".tool-badge"),
+      { opacity: [0, 1], y: ["50px", 0], scaleX: [0.6, 1], rotateX: [120, 0] },
+      { duration: 0.8, delay: 0 },
+    );
+
+    // Animate the Section Header Text
+    animate(
+      testimonialsSection?.querySelectorAll("#header-text"),
+      { opacity: [0, 1], y: ["50px", 0], scaleY: [0.4, 1] },
+      { duration: 0.8, delay: 0.4 },
+    );
+
+    // Animate the Active Testimonial Text
+    animate(
+      testimonialsSection?.querySelectorAll("#testimonial-text"),
+      { y: ["50px", 0], opacity: [0, 1] },
+      { duration: 0.9, delay: 0.6 },
+    );
+
+    // Animate the Testimonial Clients
+    animate(
+      testimonialsSection?.querySelectorAll(".testimonial-item"),
+      { opacity: [0, 1], x: ["250px", 0] },
+      { duration: 0.6, ease: "linear", delay: stagger(0.2, { from: "first", startDelay: 1 }) },
+    );
+  });
+};
