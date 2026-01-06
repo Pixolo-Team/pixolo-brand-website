435528c036fba0bd335560ca38b98d027100cc6e
Palak Lund <palaklund123@gmail.com>
2026-01-06 13:17:40 +0530

    added the animations and made the changes in the responsiveness

---
+++ src/data/projectIntroduction.ts
@@ -0,0 +1,11 @@
+export const projectIntroductionDetails = {
+  heading: "Inside the Build: A Clear and Creative Project Walkthrough",
+  image: "upscale_image.jpg",
+  aboutTheProjectHeading: "About this project",
+  aboutTheproject:
+    "FC Barcelona is a legendary football club known for its winning spirit, iconic players,and dynamic, attacking style of play. Inspired by a strong culture and global fanbase, the club continues to set standards in modern football.",
+  targetAudienceHeading: " Target Audience",
+  targetAudience: " Kids and GenZ who loves football",
+  purposeHeading: "Purpose",
+  purpose: "To help kids and parents their performance",
+};
