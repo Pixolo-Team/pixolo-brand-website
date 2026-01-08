311661c5040f5116527b0d2391c5bc154455a098
gerejakajal <gerejakajal207@gmail.com>
2026-01-08 17:58:28 +0530

    Completed Pain Solution Section

---
+++ src/data/service-consumption/PainSolution.ts
@@ -0,0 +1,30 @@
+// Retainer section details
+export const painSolutionSectionDetails = {
+  badgeIcon: "package",
+  badgeText: "Challenges We Solve",
+  headerText: "Built to Turn Roadblocks into Results",
+
+  // PainSolution Card Items details
+  painSolutionCardItems: [
+    {
+      problemDescription: "Unclear Requirements",
+      solutionDescription: "A proven Discovery Process",
+    },
+    {
+      problemDescription: "Overpromised Timelines",
+      solutionDescription: "Realistic Scoping",
+    },
+    {
+      problemDescription: "Developers Who Needs Micro-Management",
+      solutionDescription: "Transparent Timelines",
+    },
+    {
+      problemDescription: "MVPs That Falls After Launch",
+      solutionDescription: "Architecture First-Development",
+    },
+    {
+      problemDescription: "Scope Creep And Surprise Budget",
+      solutionDescription: "Fast Iteration Cycles",
+    },
+  ],
+};
