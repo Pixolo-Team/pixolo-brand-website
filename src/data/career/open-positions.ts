2bff0f630a6818e6312c7bbd700a99b924a2f9df
nagdevtaniya <nagdevtaniya@gmail.com>
2026-01-08 20:12:12 +0530

    Fully responsive Open Positions Section

--- src/data/career/open-positions.ts
+++ src/data/career/open-positions.ts
@@ -1,8 +1,121 @@
-export const openPosition = {
-  category: "Designing",
-  title: "UI/UX Designer",
-  experience: "1-2 years",
-  location: "Mumbai",
-  positions: "3-4",
-  ctaText: "Apply Now",
+export const openPositions = {
+  toolBadgeText: "Join Our Team",
+  toolBadgeIcon: "package",
+  title: "Discover roles where your skills, passion, and ideas can thrive",
+  positions: [
+    {
+      category: "Designing",
+      title: "UI/UX Designer",
+      tags: [
+        {
+          label: "Experience",
+          value: "1-2 years",
+        },
+        {
+          label: "Location",
+          value: "Mumbai",
+        },
+        {
+          label: "Open Positions",
+          value: "3-4",
+        },
+      ],
+      ctaText: "Apply Now",
+    },
+    {
+      category: "Designing",
+      title: "UI/UX Designer",
+      tags: [
+        {
+          label: "Experience",
+          value: "1-2 years",
+        },
+        {
+          label: "Location",
+          value: "Mumbai",
+        },
+        {
+          label: "Open Positions",
+          value: "3-4",
+        },
+      ],
+      ctaText: "Apply Now",
+    },
+    {
+      category: "Designing",
+      title: "UI/UX Designer",
+      tags: [
+        {
+          label: "Experience",
+          value: "1-2 years",
+        },
+        {
+          label: "Location",
+          value: "Mumbai",
+        },
+        {
+          label: "Open Positions",
+          value: "3-4",
+        },
+      ],
+      ctaText: "Apply Now",
+    },
+    {
+      category: "Designing",
+      title: "UI/UX Designer",
+      tags: [
+        {
+          label: "Experience",
+          value: "1-2 years",
+        },
+        {
+          label: "Location",
+          value: "Mumbai",
+        },
+        {
+          label: "Open Positions",
+          value: "3-4",
+        },
+      ],
+      ctaText: "Apply Now",
+    },
+    {
+      category: "Designing",
+      title: "UI/UX Designer",
+      tags: [
+        {
+          label: "Experience",
+          value: "1-2 years",
+        },
+        {
+          label: "Location",
+          value: "Mumbai",
+        },
+        {
+          label: "Open Positions",
+          value: "3-4",
+        },
+      ],
+      ctaText: "Apply Now",
+    },
+    {
+      category: "Designing",
+      title: "UI/UX Designer",
+      tags: [
+        {
+          label: "Experience",
+          value: "1-2 years",
+        },
+        {
+          label: "Location",
+          value: "Mumbai",
+        },
+        {
+          label: "Open Positions",
+          value: "3-4",
+        },
+      ],
+      ctaText: "Apply Now",
+    },
+  ],
 };
