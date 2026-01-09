8d86d3aa963dda46db46239646086d2599854831
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-01-09 16:01:32 +0530

    animations added and ui fixes

--- src/data/career/hero.ts
+++ src/data/career/hero.ts
@@ -1,4 +1,7 @@
-export const careerHeroDetails = {
-  heading: "You finally found the best place to work",
-  subHeading: "Are you up for the challenge ?",
+// Career Hero Section Details
+export const careerHeroSectionDetails = {
+  badgeIcon: "comment",
+  badgeTitle: "Build Your Future With Us",
+  title: "You finally found the best place to work",
+  subTitle: "Are you up for the challenge ?",
 };
