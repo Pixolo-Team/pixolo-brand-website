bb58f2d25cde4d763b9db8c3e55b788c0a8b5481
gerejakajal <gerejakajal207@gmail.com>
2026-01-09 19:44:38 +0530

    Fixed code review comments

--- src/data/service-consumption/outcome-focused.ts
+++ src/data/service-consumption/outcome-focused.ts
@@ -6,23 +6,18 @@ export const outcomeFocusedDetails = {
   // Why Use Details
   whyUs: [
     {
-      number: "01",
       description: "A clean Product Plan",
     },
     {
-      number: "02",
       description: "A working MVP in weeks",
     },
     {
-      number: "03",
       description: "A Scalable Architecture for Later Growth",
     },
     {
-      number: "04",
       description: "UI/UX that Users Understand Instantly",
     },
     {
-      number: "05",
       description: "Clean Code Built for Long-Term Development",
     },
   ],
