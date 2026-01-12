91825cc358e9ea53ab9591525f1b8c2e4149abb4
Kajal Gereja <gerejakajal207@gmail.com>
2026-01-12 18:12:41 +0530

    Outcome Focused Section - UI Development (#128)
    * Completed why us Component
    
    * Completed Outcome Section
    
    * added comments
    
    * Fixed Ui comments
    
    * Fixed padding as pe ui testing comment
    
    * Fixed QC
    
    * Fixed Ui
    
    * Fixed UI comments
    
    * QC fixes
    
    * Fixed UI testing comment
    
    * QC fixes
    
    * Fixed Copilot code review comments
    
    * UI fixes
    
    * Fixed code review comments
    
    * fixes
    
    * pr fixes
    
    ---------
    
    Co-authored-by: Mohdhussainsunesara <m.sunesara@somaiya.edu>

---
+++ src/data/service-consumption/outcome-focused.ts
@@ -0,0 +1,24 @@
+// Outcome Focused section details
+export const outcomeFocusedDetails = {
+  badgeText: "Your deliverables",
+  headerText: "Included in this service",
+
+  // Why Use Details
+  whyUs: [
+    {
+      description: "A clean Product Plan",
+    },
+    {
+      description: "A working MVP in weeks",
+    },
+    {
+      description: "A Scalable Architecture for Later Growth",
+    },
+    {
+      description: "UI/UX that Users Understand Instantly",
+    },
+    {
+      description: "Clean Code Built for Long-Term Development",
+    },
+  ],
+};
