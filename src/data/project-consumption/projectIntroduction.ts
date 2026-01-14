59a98ac037091401e4234814deb73d14ceb096ea
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-01-14 14:44:53 +0530

    small fixes

--- src/data/project-consumption/projectIntroduction.ts
+++ src/data/project-consumption/projectIntroduction.ts
@@ -1,6 +1,6 @@
 export const projectIntroductionDetails = {
   heading: "Inside the Build: A Clear and Creative Project Walkthrough",
-  image: "/images/project-consumption/project-introduction-default.jpg",
+  image: "/images/portfolio/project-consumption/project-introduction-default.jpg",
   aboutTheProjectHeading: "About this project",
   aboutTheproject:
     "FC Barcelona is a legendary football club known for its winning spirit, iconic players,and dynamic, attacking style of play. Inspired by a strong culture and global fanbase, the club continues to set standards in modern football.",
