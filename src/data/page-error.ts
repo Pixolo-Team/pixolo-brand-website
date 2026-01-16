31642f3cf92c8085bee35a4ed217b46541ac5da9
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-01-16 19:40:32 +0530

    pr fixes

---
+++ src/data/page-error.ts
@@ -0,0 +1,11 @@
+// Page Error Data
+export const pageErrorDetails = {
+  badgeIcon: "package",
+  badgeTitle: "404 vibes only",
+  image: "/images/astronaut.png",
+  mainImage: "/images/404.png",
+  title: "Captain, something weird just happened… again.",
+  description:
+    "Oops! You've ventured into the digital void. The page you are looking for has apparently escaped into another dimension.",
+  ctaText: "Teleport Home",
+};
