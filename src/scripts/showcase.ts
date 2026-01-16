9cb678f3ce21c16ec7e31bc0562d8a5cc0cdaa9e
Aashish Artani <aashish.artani@gmail.com>
2026-01-16 18:17:45 +0530

    handled animations of portfolio project card

--- src/scripts/showcase.ts
+++ src/scripts/showcase.ts
@@ -1,6 +1,8 @@
 // OTHERS //
 import EmblaCarousel from "embla-carousel";
+import { animate, inView, stagger } from "motion";
 
+/** Handle the Mini Project Cards Carousel Functionality **/
 export function initShowcaseSlider() {
   // Grab wrapper nodes
   const rootNode = document.querySelector(".embla");
@@ -21,6 +23,7 @@ export function initShowcaseSlider() {
     skipSnaps: false,
   });
 
+  // Checking and initializing each time event occurs
   const reInitialize = () => {
     const progress = Math.max(0, Math.min(1, embla.scrollProgress()));
     console.log(embla.scrollProgress());
@@ -32,6 +35,7 @@ export function initShowcaseSlider() {
     else prevButtonNode.children[0].classList.add("text-n-500");
   };
 
+  // Remove Event Listeners Function
   const removePrevNextBtnsClickHandlers = () => {
     nextButtonNode.removeEventListener(
       "click",
@@ -63,6 +67,7 @@ export function initShowcaseSlider() {
     reInitialize();
   });
 
+  // Initializing the events of embla to handle progress bar and slides
   embla
     .on("init", reInitialize)
     .on("reInit", reInitialize)
@@ -74,27 +79,111 @@ export function initShowcaseSlider() {
     .on("destroy", removePrevNextBtnsClickHandlers);
 }
 
+/** Animate the Showcase Section **/
+export const animateShowcaseSection = () => {
+  // Detect when the Section comes into the Screen
+  inView("#showcase-section", (showcaseSection) => {
+    // Animate the Section Header Part
+    animate(
+      showcaseSection?.querySelectorAll(".tool-badge"),
+      { opacity: [0, 1], y: ["50px", 0], scaleX: [0.6, 1], rotateX: [120, 0] },
+      { duration: 0.8, delay: 0.2 },
+    );
+
+    animate(
+      showcaseSection?.querySelectorAll("#header-text"),
+      { opacity: [0, 1], y: ["50px", 0], scaleY: [0.4, 1] },
+      { duration: 0.8, delay: 0.6 },
+    );
+  });
+};
+
+export const animateProjectCards = () => {
+  // Detect when the Project Card comes into the Screen //
+  inView("#portfolio-showcase-project-card", (card) => {
+    // Extracting the Project Card Elements //
+    const image = card?.querySelectorAll(".project-card-image");
+    const title = card?.querySelectorAll(".project-card-title");
+    const badges = card?.querySelectorAll(".project-card-badges");
+    const texts = card?.querySelectorAll(
+      ".project-card-subtitle, .project-card-description, .project-card-date",
+    );
+    const buttons = card?.querySelectorAll(".project-card-buttons");
+
+    if (!card || !image || !title || !badges || !texts || !buttons) {
+      console.log("error");
+      return;
+    }
+
+    // Animate the Project Card Elements //
+    animate(
+      image,
+      { opacity: [0, 1], x: ["50px", 0] },
+      {
+        duration: 0.5,
+        delay: 0.2,
+      },
+    );
+
+    animate(
+      title,
+      { opacity: [0, 1], x: ["30px", 0] },
+      {
+        duration: 0.2,
+        delay: 0.6,
+      },
+    );
+
+    animate(
+      badges,
+      { opacity: [0, 1], x: ["30px", 0] },
+      {
+        duration: 0.2,
+        delay: 0.8,
+      },
+    );
+
+    animate(
+      Array.from(texts),
+      { opacity: [0, 1], x: ["30px", 0] },
+      {
+        duration: 0.6,
+        delay: stagger(0.2, { startDelay: 1 }),
+      },
+    );
+
+    animate(
+      buttons,
+      { opacity: [0, 1], x: [20, 0] },
+      {
+        duration: 0.2,
+        delay: 1.8,
+      },
+    );
+  });
+};
+
+/** Handling the Toggle Functionality of Project Cards View **/
 let view = "list";
-
 export function initShowcase() {
-  const gridIcon = document.getElementById("gridIcon");
-  const listIcon = document.getElementById("listIcon");
+  const gridIconBtn = document.getElementById("gridIconBtn");
+  const listIconBtn = document.getElementById("listIconBtn");
+  const rootNode = document.getElementById("portfolio-project-card");
 
-  if (!gridIcon || !listIcon) return;
+  if (!gridIconBtn || !listIconBtn || !rootNode) return;
 
-  gridIcon.addEventListener("click", () => {
-    view = "grid";
+  view = rootNode.getAttribute("data-view") === "list" ? "list" : "grid";
+  gridIconBtn.addEventListener("click", () => {
+    rootNode.setAttribute("data-view", "grid");
     location.reload();
-    console.log(view);
   });
 
-  listIcon.addEventListener("click", () => {
-    view = "list";
+  listIconBtn.addEventListener("click", () => {
+    rootNode.setAttribute("data-view", "list");
     location.reload();
-    console.log(view);
   });
 }
 
 export function getView() {
-  return view == "list" ? "list" : "grid";
+  return view === "list" ? "list" : "grid";
 }
