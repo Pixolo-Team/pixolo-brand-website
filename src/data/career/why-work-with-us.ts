6e32473dfc2c40a2a1f4915c1a3f3a25eb8bf763
Palak Lund <palaklund123@gmail.com>
2026-01-16 17:45:58 +0530

    made the UI for this section

---
+++ src/data/career/why-work-with-us.ts
@@ -0,0 +1,42 @@
+export const reasonToWorkWithUs = [
+  {
+    id: "01",
+    title: "Ownership Over Work And Decisions",
+    hiddeTitle: "Pages load instantly, keeping visitors engaged instead of frustrated",
+  },
+  {
+    id: "02",
+    title: "Creative Freedom",
+    hiddeTitle: "Pages load instantly, keeping visitors engaged instead of frustrated",
+  },
+  {
+    id: "03",
+    title: "Learning By Doing, Not Watching",
+    hiddeTitle: "Pages load instantly, keeping visitors engaged instead of frustrated",
+  },
+  {
+    id: "04",
+    title: "Clear, Honest Feedback",
+    hiddeTitle: "Pages load instantly, keeping visitors engaged instead of frustrated",
+  },
+  {
+    id: "05",
+    title: "High Standards, Real Support",
+    hiddeTitle: "Pages load instantly, keeping visitors engaged instead of frustrated",
+  },
+  {
+    id: "06",
+    title: "Leadership Isn’t A Title Here",
+    hiddeTitle: "Pages load instantly, keeping visitors engaged instead of frustrated",
+  },
+  {
+    id: "07",
+    title: "People First, Always",
+    hiddeTitle: "Pages load instantly, keeping visitors engaged instead of frustrated",
+  },
+];
+export const headingsAndDetails = {
+  image: "/images/career/why-work-with-us/team-image.jpg",
+  badgeText: "Your Growth Starts Here",
+  mainTitle: "A friendly, flexible environment built for your best work.",
+};
