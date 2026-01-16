eb3895036203c5c8d8bc52c55dea4dbc1653d51d
Jayant Singh <jayantsinghhh89@gmail.com>
2026-01-16 16:15:41 +0530

    Animations fixed in new script file

--- src/scripts/blog-consumption.ts
+++ src/scripts/blog-consumption.ts
@@ -1,3 +1,36 @@
 // PLUGINS //
 import { animate, inView, stagger } from "motion";
 
+// Animate Blog Body Header to fade in up
+export const animateHeaderFadeInUp = () => {
+  inView(".header-anim", (element) => {
+    animate(
+      element,
+      {
+        opacity: [0, 1],
+        y: [20, 0],
+      },
+      {
+        duration: 0.8,
+        easing: "ease-in-out",
+      },
+    );
+  });
+};
+
+// Animate Image & Video Block to fade in up quickly
+export const animateMediaFadeInUp = () => {
+  inView(".media-anim", (element) => {
+    animate(
+      element,
+      {
+        opacity: [0, 1],
+        y: [20, 0],
+      },
+      {
+        duration: 0.6,
+        easing: "ease-out",
+      },
+    );
+  });
+};
