c24a44170ad07574ade70b6815c9926db3ae6e80
nagdevtaniya <nagdevtaniya@gmail.com>
2026-01-19 21:05:15 +0530

    Images

--- src/data/career/belonging-statement.ts
+++ src/data/career/belonging-statement.ts
@@ -4,28 +4,28 @@ export const belongingStatement = {
   title: "A place where everyone belongs, grows, and shines",
   belongingStatementCard: [
     {
-      imageSrc: "/images/career/coffee-image.jpg",
+      imageSrc: "/images/career/character-over-credentials.jpg",
       imageAlt: "Coffee Image",
       heading: "Character Over Credentials",
       description:
         "Skill matters. Smartness matters. But none of it is worth anything without humility, empathy, and a good heart. We hire people with integrity who can learn fast, collaborate well, and never mistake talent for entitlement. We’ve brought in folks from unusual paths — like a footballer learning design — because potential and attitude matter more than a perfect resume.",
     },
     {
-      imageSrc: "/images/career/office-image.jpg",
+      imageSrc: "/images/career/zero-tolerance.jpg",
       imageAlt: "Office Image",
       heading: "Zero Tolerance for Anything That Breaks Trust",
       description:
         "Trust is the backbone here. We don’t compromise on honesty, respect for the vision, or commitment to the team. Lying, stealing, half-hearted effort, ego trips, or laziness aren’t brushed aside. We protect the culture, even when it’s uncomfortable.",
     },
     {
-      imageSrc: "/images/career/party-image.jpg",
+      imageSrc: "/images/career/every-idea-matters.jpg",
       imageAlt: "Party Image",
       heading: "Every Voice Matters, Every Idea Is Welcome",
       description:
         "Pixolo should be the place where creativity stays alive. Juniors can question decisions, challenge assumptions, and pitch ideas without worrying about hierarchy. People are encouraged to speak, experiment, and influence the direction of our work. Everyone deserves to feel heard and valued.",
     },
     {
-      imageSrc: "/images/career/omkar-image.jpg",
+      imageSrc: "/images/career/growth-without-gatekeeping.jpg",
       imageAlt: "Omkar Image",
       heading: "Growth Without Gatekeeping",
       description:
