feaa76d71ac5f2efc0670fb7ff41a960b7a57136
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-01-20 21:01:57 +0530

    form addition

--- src/data/career/open-positions.ts
+++ src/data/career/open-positions.ts
@@ -4,8 +4,8 @@ export const openPositions = {
   title: "Discover roles where your skills, passion, and ideas can thrive",
   positions: [
     {
-      category: "Designing",
-      title: "UI/UX Designer",
+      category: "Sales",
+      title: "Sales Executive",
       tags: [
         {
           label: "Experience",
@@ -17,97 +17,7 @@ export const openPositions = {
         },
         {
           label: "Open Positions",
-          value: "3-4",
-        },
-      ],
-    },
-    {
-      category: "Designing",
-      title: "UI/UX Designer",
-      tags: [
-        {
-          label: "Experience",
-          value: "1-2 years",
-        },
-        {
-          label: "Location",
-          value: "Mumbai",
-        },
-        {
-          label: "Open Positions",
-          value: "3-4",
-        },
-      ],
-    },
-    {
-      category: "Designing",
-      title: "UI/UX Designer",
-      tags: [
-        {
-          label: "Experience",
-          value: "1-2 years",
-        },
-        {
-          label: "Location",
-          value: "Mumbai",
-        },
-        {
-          label: "Open Positions",
-          value: "3-4",
-        },
-      ],
-    },
-    {
-      category: "Designing",
-      title: "Front-end Developer",
-      tags: [
-        {
-          label: "Experience",
-          value: "3-4 years",
-        },
-        {
-          label: "Location",
-          value: "Bangalore",
-        },
-        {
-          label: "Open Positions",
-          value: "5-6",
-        },
-      ],
-    },
-    {
-      category: "Testing",
-      title: "QA Engineer",
-      tags: [
-        {
-          label: "Experience",
-          value: "1-3 years",
-        },
-        {
-          label: "Location",
-          value: "Pune",
-        },
-        {
-          label: "Open Positions",
-          value: "2-3",
-        },
-      ],
-    },
-    {
-      category: "Managing",
-      title: "Project Manager",
-      tags: [
-        {
-          label: "Experience",
-          value: "5+ years",
-        },
-        {
-          label: "Location",
-          value: "Delhi",
-        },
-        {
-          label: "Open Positions",
-          value: "1-2",
+          value: "1",
         },
       ],
     },
