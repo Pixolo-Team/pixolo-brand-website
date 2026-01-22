03ac253b73f4cc3ed319369ffdfb5a6dbd284483
nagdevtaniya <nagdevtaniya@gmail.com>
2026-01-22 17:01:29 +0530

    ProuctItem Image update and hover fixes (#176)
    * ProuctItem Image update and hover fixes
    
    * Services button fix
    
    * Loading number issue
    
    * fixes
    
    ---------
    
    Co-authored-by: Mohdhussainsunesara <m.sunesara@somaiya.edu>

--- src/data/products.ts
+++ src/data/products.ts
@@ -15,8 +15,8 @@ export const productItems = [
     thumbnail: "zizo-thumbnail.jpg",
     name: "Zizo",
     description:
-      "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
-    href: "/",
+      "A smart football tournament listing app that helps coaches, players and clubs to explore & participate in football Tournaments.",
+    href: "https://www.zizoapp.in/football-tournaments",
   },
   {
     darkLogo: "configo-logo.png",
@@ -25,7 +25,6 @@ export const productItems = [
     name: "Configo",
     description:
       " A single control panel for all your DevOps needs. Instead of juggling servers across multiple platforms, Configo centralizes your databases, cloud servers, deployments, and access management in one clean, managed space.",
-    href: "/",
   },
   {
     darkLogo: "weddingo-logo.png",
@@ -34,7 +33,6 @@ export const productItems = [
     name: "WeddinGo",
     description:
       "Weddingo is a digital platform for modern weddings — invitations, event timelines, RSVP tracking, and photo galleries all in one place. It gives couples a celebrity-style experience without the traditional chaos.",
-    href: "/",
   },
   {
     darkLogo: "neevo-logo.png",
@@ -43,6 +41,5 @@ export const productItems = [
     name: "Neevo Design System",
     description:
       " Neevo is a clean, flexible design system with ready-to-use components that any brand can adapt to their style in minutes. It helps teams build consistent interfaces fast, even without deep UI/UX expertise. Everything updates automatically with your brand colors, spacing, and typography.",
-    href: "/",
   },
 ];
