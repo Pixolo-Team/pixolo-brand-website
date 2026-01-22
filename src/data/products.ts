6e1956cc6c3c85dac3a8546ab523f83533141202
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-01-22 16:55:25 +0530

    fixes

--- src/data/products.ts
+++ src/data/products.ts
@@ -14,8 +14,8 @@ export const productItems = [
     thumbnail: "zizo-thumbnail.jpg",
     name: "Zizo",
     description:
-      "A smart football team management app that helps coaches, players, and clubs organize matches, track stats, and manage their teams seamlessly.",
-    href: "/",
+      "A smart football tournament listing app that helps coaches, players and clubs to explore & participate in football Tournaments.",
+    href: "https://www.zizoapp.in/football-tournaments",
   },
   {
     logo: "configo-logo.png",
@@ -23,7 +23,6 @@ export const productItems = [
     name: "Configo",
     description:
       " A single control panel for all your DevOps needs. Instead of juggling servers across multiple platforms, Configo centralizes your databases, cloud servers, deployments, and access management in one clean, managed space.",
-    href: "/",
   },
   {
     logo: "weddingo-logo.png",
@@ -31,7 +30,6 @@ export const productItems = [
     name: "WeddinGo",
     description:
       "Weddingo is a digital platform for modern weddings — invitations, event timelines, RSVP tracking, and photo galleries all in one place. It gives couples a celebrity-style experience without the traditional chaos.",
-    href: "/",
   },
   {
     logo: "neevo-logo.png",
@@ -39,6 +37,5 @@ export const productItems = [
     name: "Neevo Design System",
     description:
       " Neevo is a clean, flexible design system with ready-to-use components that any brand can adapt to their style in minutes. It helps teams build consistent interfaces fast, even without deep UI/UX expertise. Everything updates automatically with your brand colors, spacing, and typography.",
-    href: "/",
   },
 ];
