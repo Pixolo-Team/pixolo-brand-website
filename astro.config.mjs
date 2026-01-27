d4e6b70cf313b14b8522e7e2feeca19fac5ea7c8
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-01-27 16:13:39 +0530

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
-// MODULES //
-import path from "path";
-
 // OTHERS //
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.pixolotechnologies.com",
+  integrations: [icon()],
+
   vite: {
     resolve: {
       alias: {
@@ -19,5 +19,4 @@ export default defineConfig({
     },
     plugins: [tailwindcss(), sitemap()],
   },
-  integrations: [icon()],
 });
