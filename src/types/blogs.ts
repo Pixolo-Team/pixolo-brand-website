ccea5a9dca398e987c61be7017768b63fe1c3542
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-01-29 17:05:54 +0530

    fixes

--- src/types/blogs.ts
+++ src/types/blogs.ts
@@ -1,23 +1,11 @@
 // TYPE FOR BLOG CONSUMPTION
 export type BlogPost = {
   title: string;
-
-  category: {
-    label: string;
-    type: string;
-  };
-
-  author: {
-    name: string;
-  };
-
-  publishedAt: string;
-
-  hero: {
-    image: string;
-    alt: string;
-  };
-
+  category: string;
+  author: string;
+  date: string;
+  image: string;
+  alt: string;
   content: BlogBlock[];
 };
 
