bb6678cf703c9941f712bbb0ffa3f201ea91b177
Palak Lund <palaklund123@gmail.com>
2026-01-30 19:44:49 +0530

    integrated the API

---
+++ src/types/portfolio-showcase.types.ts
@@ -0,0 +1,13 @@
+export type PortfolioShowcaseData = {
+  id: string;
+  brand_color: string;
+  brand_name: string;
+  hero_image: string;
+  thumbnail_image: string;
+  logo: string;
+  title: string;
+  subtitle: string;
+  badges: string[];
+  show_in_home: string;
+  is_highlighted: string;
+};
