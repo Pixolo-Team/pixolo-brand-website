4625bcb991e3c1b7ffa4313cf54c0e2cbde7d424
nagdevtaniya <nagdevtaniya@gmail.com>
2026-01-31 16:48:02 +0530

    Dynamic data (#195)
    * Dynamic data
    
    * pr fixes
    
    ---------
    
    Co-authored-by: Mohdhussainsunesara <m.sunesara@somaiya.edu>

---
+++ src/types/portfolio.ts
@@ -0,0 +1,13 @@
+// Types for Portfolio
+export type PortfolioData = {
+  brandColor: string;
+  brandName: string;
+  heroImage: string;
+  thumbnailImage: string;
+  logo: string;
+  title: string;
+  subtitle: string;
+  badges: string[];
+  showInHome: "TRUE" | "FALSE";
+  isHightlight: "TRUE" | "FALSE";
+};
