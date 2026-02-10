d73dd1bda00c914ffb549f3075c1b38a86ded1b2
Mohd hussain <90891392+MohdhussainSunesara@users.noreply.github.com>
2026-02-10 19:02:19 +0530

    Portfolio Consumption - Client Testimonial (#214)
    * section added
    
    * section added

--- src/utils/site-url.ts
+++ src/utils/site-url.ts
@@ -5,5 +5,5 @@
  * @returns {string} The site URL
  */
 export const getSiteUrl = () => {
-  return import.meta.env.LOCAL_SITE_URL?.replace(/\/$/, "") || "https://pixolotechnologies.com";
+  return import.meta.env.ROOT_URL?.replace(/\/$/, "") || "https://pixolotechnologies.com";
 };
