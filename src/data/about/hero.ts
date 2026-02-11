d284c4aa8859a20f3776ac02f2431d948604e224
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-02-11 15:08:37 +0530

    about hero section

---
+++ src/data/about/hero.ts
@@ -0,0 +1,7 @@
+// Career Hero Section Details
+export const aboutHeroSectionDetails = {
+  badgeIcon: "comment",
+  badgeTitle: "About Pixolo",
+  title: "We help founders build reliable software.",
+  subTitle: "Pixolo removes uncertainty from product development.",
+};
