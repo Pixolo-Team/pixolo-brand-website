5101258b353ec8be394a25bf9c9383b97e1afc47
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-02-11 19:19:52 +0530

    teams section

---
+++ src/data/about/teams.ts
@@ -0,0 +1,49 @@
+// Team section details
+export const teamsSectionDetails = {
+  badgeIcon: "user-group",
+  badgeTitle: "Team",
+  title: "The people behind Pixolo",
+
+  teamCardItems: [
+    {
+      name: "Abhay",
+      role: "Founder & CTO",
+      profileImage: "/images/team/abhay.jpg",
+      quote: "Thinks in",
+      quoteHighlight: "systems and football formations",
+      avatarImage: "/images/team/abhay-amin-face.jpg",
+      linkedinUrl: "",
+      instagramUrl: "",
+    },
+    {
+      name: "Abhay",
+      role: "Founder & CTO",
+      profileImage: "/images/team/abhay.jpg",
+      quote: "Thinks in",
+      quoteHighlight: "systems and football formations",
+      avatarImage: "/images/team/abhay-amin-face.jpg",
+      linkedinUrl: "",
+      instagramUrl: "",
+    },
+    {
+      name: "Abhay",
+      role: "Founder & CTO",
+      profileImage: "/images/team/abhay.jpg",
+      quote: "Thinks in",
+      quoteHighlight: "systems and football formations",
+      avatarImage: "/images/team/abhay-amin-face.jpg",
+      linkedinUrl: "",
+      instagramUrl: "",
+    },
+    {
+      name: "Abhay",
+      role: "Founder & CTO",
+      profileImage: "/images/team/abhay.jpg",
+      quote: "Thinks in",
+      quoteHighlight: "systems and football formations",
+      avatarImage: "/images/team/abhay-amin-face.jpg",
+      linkedinUrl: "",
+      instagramUrl: "",
+    },
+  ],
+};
