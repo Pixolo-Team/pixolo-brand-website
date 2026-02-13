8869d1d2fbb2cbf71374d99057acdf19a852eab7
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-02-13 16:37:16 +0530

    fixes

--- src/data/about/bts.ts
+++ src/data/about/bts.ts
@@ -2,4 +2,35 @@
 export const btsSectionDetails = {
   badgeTitle: "Behind the Scenes",
   title: "The Team, Unfiltered",
+
+  btsImages: [
+    {
+      src: "/images/about/bts-1.jpg",
+      alt: "Bts Image 1",
+    },
+    {
+      src: "/images/about/bts-2.jpg",
+      alt: "Bts Image 2",
+    },
+    {
+      src: "/images/about/bts-3.jpg",
+      alt: "Bts Image 3",
+    },
+    {
+      src: "/images/about/bts-4.jpg",
+      alt: "Bts Image 4",
+    },
+    {
+      src: "/images/about/bts-5.jpg",
+      alt: "Bts Image 5",
+    },
+    {
+      src: "/images/about/bts-6.jpg",
+      alt: "Bts Image 6",
+    },
+    {
+      src: "/images/about/bts-7.jpg",
+      alt: "Bts Image 7",
+    },
+  ],
 };
