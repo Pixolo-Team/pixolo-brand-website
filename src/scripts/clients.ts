b675670d8e615a0ac404cf0dd81509476a0b9abe
MohdhussainSunesara <m.sunesara@somaiya.edu>
2026-08-14 18:31:19 +0530

    Apply remaining homepage fixes

--- src/scripts/clients.ts
+++ src/scripts/clients.ts
@@ -22,7 +22,7 @@ export const animateClientLogos = () => {
     animate(
       clientLogoSection?.querySelectorAll(".client-logo"),
       { y: ["50px", 0], opacity: [0, 1] },
-      { duration: 0.4, delay: stagger(0.2, { from: "first", startDelay: 1 }) },
+      { duration: 0.2, delay: stagger(0.2, { from: "first", startDelay: 1 }) },
     ).finished.then(() => {
       // Animate the Footer Text
       animate(
